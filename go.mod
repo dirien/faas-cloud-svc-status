181f50f01785191448588f09628610d8a929bfe6
Engin Diri <engin.diri@mail.schwarz>
2021-05-16 18:01:10 +0200

    initial commit (#1)
    * initial commit
    
    * initial commit

---
+++ go.mod
@@ -0,0 +1,8 @@
+module faas-cloud-svc-status
+
+go 1.16
+
+require (
+	github.com/dirien/status-keeper v0.0.2
+	github.com/openfaas/templates-sdk v0.0.0-20200723110415-a699ec277c12
+)
---
+++ main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"github.com/dirien/status-keeper/statuspage"
+	"sort"
+)
+
+func main() {
+	statusPages := statuspage.GetStatusPages()
+	sort.Sort(statusPages)
+	name := "STACKIT"
+	var status *statuspage.StatusPage
+
+	for _, t := range statusPages {
+		if t.Name == name {
+			status = &t
+			break
+		}
+	}
+
+	if status == nil {
+		fmt.Println(fmt.Errorf("cannot get status: %s", name))
+		return
+	}
+
+	fmt.Printf("Status %s\n", status.Name)
+	fmt.Println("here")
+	resp, err := statuspage.Download(status)
+	fmt.Println("here2")
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+	fmt.Println(resp)
+
+}
